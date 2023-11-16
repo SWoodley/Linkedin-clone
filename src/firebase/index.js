f4bedc9e0b61664ec366ee908ed716c5f5b15430
SWoodley <swoodley2@fordham.edu>
2023-11-16 18:43:39 -0500

    n/a

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,9 +2,15 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
-import { environmentConfig } from "./environmentConfig";
 
-const configFile = environmentConfig;
+const configFile = {
+    apiKey: `${process.env.API_KEY}`,
+    authDomain: `${process.env.AUTH_DOMAIN}`,
+    projectId: `${process.env.PROJECT_ID}`,
+    storageBucket: `${process.env.STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+    appId: `${process.env.APP_ID}`
+}
 
 firebase.initializeApp(configFile);
 const db = firebase.firestore();
