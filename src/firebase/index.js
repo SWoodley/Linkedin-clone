660ceabe9285948351ba8a92f8a711f6f1f50846
SWoodley <swoodley2@fordham.edu>
2023-11-16 21:56:25 -0500

    Added react_app prefix to env vars

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,16 +3,17 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-const configFile = {
-    apiKey: `${process.env.API_KEY}`,
-    authDomain: `${process.env.AUTH_DOMAIN}`,
-    projectId: `${process.env.PROJECT_ID}`,
-    storageBucket: `${process.env.STORAGE_BUCKET}`,
-    messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
-    appId: `${process.env.APP_ID}`
+const firebaseConfig = {
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_APP_ID}`
 };
 
-firebase.initializeApp(configFile);
+console.log(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
